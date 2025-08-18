151cb46759810c53639a4f04f688d5a203dedba6
maeri <onyonkamaeriofficial@gmail.com>
2025-08-18 07:58:43 +0300

    Initial commit

---
+++ src/pages/dashboard/components/am4chartMap/mock.js
@@ -0,0 +1,69 @@
+export default [
+  {
+    latitude: -1.3028603,
+    longitude: 36.6822393,
+    size: 6,
+    tooltip: 'Nairobi',
+    fill: "#F00"
+  },
+  {
+    latitude: -0.5388369,
+    longitude: 34.4074833,
+    size: 7,
+    tooltip: 'HomaBay',
+  },
+  {
+    latitude: -1.3028603,
+    longitude: 36.6822393,
+    size: 4,
+    tooltip: 'San Francisco',
+  },
+  {
+    latitude: -0.4590622,
+    longitude: 36.1692277,
+    size: 9,
+    tooltip: 'Lake Okeechobee',
+  },
+  {
+    latitude:  -0.5312646,
+    longitude: 34.4714521,
+    size: 4,
+    tooltip: 'Grainger County',
+  },
+  {
+    latitude: 0.0579555,
+    longitude: 35.744922,
+    size: 8,
+    tooltip: 'Chowan County',
+  },
+  {
+    latitude: 0.8357708,
+    longitude: 36.6845348,
+    size: 6,
+    tooltip: 'Lake Livingston',
+  },
+  {
+    latitude: 3.9351574,
+    longitude: 41.8340422,
+    size: 5,
+    tooltip: 'Tunica County',
+  },
+  {
+    latitude: 3.5991762,
+    longitude: 40.2240811,
+    size: 5,
+    tooltip: 'Lea County',
+  },
+  {
+    latitude: 40.456692,
+    longitude: -83.522688,
+    size: 5,
+    tooltip: 'Union County',
+  },
+  {
+    latitude: 33.844630,
+    longitude: -118.157483,
+    size: 6,
+    tooltip: 'Lakewood Mutual',
+  }
+]
